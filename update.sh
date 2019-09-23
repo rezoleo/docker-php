de12b7e60278b49df44bb218dcc5783588504318
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2019-09-23 20:43:38 +0200

    actions: Fix git username & email

--- update.sh
+++ update.sh
@@ -7,6 +7,9 @@ for version in "${versions[@]}"; do
   git add "${version}-apache/Dockerfile"
 done
 
+git config user.email "contact@rezoleo.fr"
+git config user.name "Rézoléo Bot"
+
 git commit -m "(actions) Updated Dockerfiles"
 git remote set-url --push origin "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
-git push
+git push HEAD:"${GITHUB_REF}"
